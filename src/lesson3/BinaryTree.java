6869546fb97090cc0ff63d45cd28c52135173430
ilyaprogger <ilyaprogger>
2019-11-12 22:43:08 +0300

    1,2 исправил коментарии

--- src/lesson3/BinaryTree.java
+++ src/lesson3/BinaryTree.java
@@ -16,9 +16,12 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
 
         Node<T> right = null;
 
+        //  Node<T> parent = null;
+
         Node(T value) {
             this.value = value;
         }
+
     }
 
     private Node<T> root = null;
@@ -35,12 +38,10 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         Node<T> newNode = new Node<>(t);
         if (closest == null) {
             root = newNode;
-        }
-        else if (comparison < 0) {
+        } else if (comparison < 0) {
             assert closest.left == null;
             closest.left = newNode;
-        }
-        else {
+        } else {
             assert closest.right == null;
             closest.right = newNode;
         }
@@ -68,14 +69,63 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         return 1 + Math.max(height(node.left), height(node.right));
     }
 
+    //@Override
+    //public boolean equals(Object obj) {
+    //    if (this == obj)
+    //        return true;
+    //    if (obj == null)
+    //        return false;
+    //    return getClass() == obj.getClass();
+    //}
+
     /**
      * Удаление элемента в дереве
      * Средняя
      */
+    //Лучший случай быстродействия O(log(n)) худший О(height()).
     @Override
     public boolean remove(Object o) {
-        // TODO
-        throw new NotImplementedError();
+        T t = (T) o;
+        if (find(t) == null) return false;
+        size--;
+        root = remove(root, new Node<>(t));
+        return true;
+    }
+
+    private Node<T> remove(Node<T> start, Node<T> delete) {
+        if (start == null) return null;
+        int a = delete.value.compareTo(start.value);
+        if (a < 0)
+            start.left = remove(start.left, delete);
+        else if (a > 0)
+            start.right = remove(start.right, delete);
+        else if (start.left != null && start.right != null) {
+            Node<T> node = new Node<>(min(start.right));
+            node.left = start.left;
+            node.right = start.right;
+            start = node;
+            start.right = remove(start.right, start);
+        } else {
+            if (start.left != null) return start.left;
+            else return start.right;
+        }
+        return start;
+    }
+
+    public T min(Node<T> node) {
+        Node<T> current = node;
+        while (current.left != null) {
+            current = current.left;
+        }
+        return current.value;
+    }
+
+    public T max(Node<T> node) {
+        Node<T> current = node;
+        while (current.right != null) {
+            current = current.right;
+        }
+        return current.value;
     }
 
     @Override
@@ -95,12 +145,10 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         int comparison = value.compareTo(start.value);
         if (comparison == 0) {
             return start;
-        }
-        else if (comparison < 0) {
+        } else if (comparison < 0) {
             if (start.left == null) return start;
             return find(start.left, value);
-        }
-        else {
+        } else {
             if (start.right == null) return start;
             return find(start.right, value);
         }
