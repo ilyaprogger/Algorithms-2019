dfb1f049f5436849c7a7faa3640c3fbd837f836a
ilyaprogger <ilyaprogger>
2019-12-06 21:26:28 +0300

    6,7

--- src/lesson6/JavaDynamicTasks.java
+++ src/lesson6/JavaDynamicTasks.java
@@ -9,7 +9,7 @@ public class JavaDynamicTasks {
     /**
      * Наибольшая общая подпоследовательность.
      * Средняя
-     *
+     * <p>
      * Дано две строки, например "nematode knowledge" и "empty bottle".
      * Найти их самую длинную общую подпоследовательность -- в примере это "emt ole".
      * Подпоследовательность отличается от подстроки тем, что её символы не обязаны идти подряд
@@ -17,15 +17,40 @@ public class JavaDynamicTasks {
      * Если общей подпоследовательности нет, вернуть пустую строку.
      * Если есть несколько самых длинных общих подпоследовательностей, вернуть любую из них.
      * При сравнении подстрок, регистр символов *имеет* значение.
-     */
+     */ //сложностью O(nm),память O(nm) , где n, m — размеры строк.
     public static String longestCommonSubSequence(String first, String second) {
-        throw new NotImplementedError();
+        int[][] table = new int[first.length() + 1][second.length() + 1];
+        StringBuilder s = new StringBuilder();
+        for (int i = first.length() - 1; i >= 0; i--) {
+            for (int j = second.length() - 1; j >= 0; j--) {
+                if (first.charAt(i) == second.charAt(j)) {
+                    table[i][j] = 1 + table[i + 1][j + 1];
+                } else {
+                    table[i][j] = Math.max(table[i + 1][j], table[i][j + 1]);
+                }
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
+        while (table[i][j] != 0 && i < first.length() && j < second.length()) {
+            if (first.charAt(i) == second.charAt(j)) {
+                result.append(first.charAt(i));
+                i++;
+                j++;
+            } else {
+                if (table[i][j] == table[i + 1][j])
+                    i++;
+                else
+                    j++;
+            }
+        }
+        return result.toString();
     }
 
     /**
      * Наибольшая возрастающая подпоследовательность
      * Сложная
-     *
+     * <p>
      * Дан список целых чисел, например, [2 8 5 9 12 6].
      * Найти в нём самую длинную возрастающую подпоследовательность.
      * Элементы подпоследовательности не обязаны идти подряд,
@@ -41,21 +66,21 @@ public class JavaDynamicTasks {
     /**
      * Самый короткий маршрут на прямоугольном поле.
      * Средняя
-     *
+     * <p>
      * В файле с именем inputName задано прямоугольное поле:
-     *
+     * <p>
      * 0 2 3 2 4 1
      * 1 5 3 4 6 2
      * 2 6 2 5 1 3
      * 1 4 3 2 6 2
      * 4 2 3 1 5 0
-     *
+     * <p>
      * Можно совершать шаги длиной в одну клетку вправо, вниз или по диагонали вправо-вниз.
      * В каждой клетке записано некоторое натуральное число или нуль.
      * Необходимо попасть из верхней левой клетки в правую нижнюю.
      * Вес маршрута вычисляется как сумма чисел со всех посещенных клеток.
      * Необходимо найти маршрут с минимальным весом и вернуть этот минимальный вес.
-     *
+     * <p>
      * Здесь ответ 2 + 3 + 4 + 1 + 2 = 12
      */
     public static int shortestPathOnField(String inputName) {
--- test/lesson6/AbstractDynamicTests.kt
+++ test/lesson6/AbstractDynamicTests.kt
@@ -4,11 +4,12 @@ import kotlin.test.assertEquals
 
 abstract class AbstractDynamicTests {
     fun longestCommonSubSequence(longestCommonSubSequence: (String, String) -> String) {
-        assertEquals("", longestCommonSubSequence("мой мир", "я"))
+        assertEquals("", longestCommonSubSequence("мой мир", ""))
         assertEquals("1", longestCommonSubSequence("1", "1"))
         assertEquals("13", longestCommonSubSequence("123", "13"))
         assertEquals("здс", longestCommonSubSequence("здравствуй мир", "мы здесь"))
         assertEquals("emt ole", longestCommonSubSequence("nematode knowledge", "empty bottle"))
+        assertEquals("            ", longestCommonSubSequence("      we      ", "                      "))
         val expectedLength = "e kerwelkkd r".length
         assertEquals(
             expectedLength, longestCommonSubSequence(
